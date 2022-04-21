71d6f8823e44407bd1508ba222b8273e825b3355
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-04-21 16:00:12 +0800

    lifecycle hooks

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,19 +1,37 @@
 <template>
-  <div class="block" >
+  <div class="block" v-if='showBlock'>
       Click Me
   </div>
 </template>
 
 <script>
 export default {
-props: ['delay']
+props: ['delay'],
+data(){
+    return{
+        showBlock: false
+    }
+},
+mounted(){
+    console.log('component mounted')
+    setTimeout(() => {
+        this.showBlock = true
+        console.log(this.delay)
+    }, this.delay)
+},
+// updated(){
+//     console.log('component updated')
+// },
+// unmounted(){
+//     console.log('component unmounted')
+// }
+
 }
 </script>
 
 <style>
 .block{
     width: 50%;
-    
     background: salmon;
     color: white;
     margin: 40px auto;
